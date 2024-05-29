b31003630650adbad6b8a4bd6942cd48d42b79b1
fjmg84 <fjmgqba@gmail.com>
2024-05-29 08:52:34 -0500

    create mobile navbar

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -107,3 +107,11 @@ export function IconX(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function IconMenu(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+      <path fill="currentColor" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z" />
+    </svg>
+  );
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const PAGES = {
+  HOME: "/",
+  ABOUT_US: "/about-us",
+};

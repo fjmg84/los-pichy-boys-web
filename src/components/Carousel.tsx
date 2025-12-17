40527050ec4dac5c45b977c3181f3f6bf7346fbb
fjmg1984 <fjmiranda@tetraimpacts.com>
2025-12-17 16:01:44 -0500

    refactor carousel component & extract reusable Title component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,59 +1,65 @@
 import Slider from "react-slick";
 import "@justinribeiro/lite-youtube";
-import LiteYouTube from "./VideoYoutube";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-function Carousel() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
+interface LiteYouTubeProps {
+  id: string;
+  title: string;
+}
+
+const LiteYouTube = ({ id, title }: LiteYouTubeProps) => (
+  <div className="mx-4">
+    <LiteYouTubeEmbed id={id} aspectHeight={9} title={title ?? ""} />
+  </div>
+);
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
+function Carousel() {
   return (
-    <div className="lg:w-[1920px] md:w-[600px] w-[300px] flex flex-col gap-16">
-      <div className="flex w-full justify-center">
-        <h1 className="font-extrabold text-yellow-500 uppercase text-2xl md:text-6xl">
-          Nuestros videos
-        </h1>
-      </div>
-      <Slider {...settings}>
-        <LiteYouTube id="Iw-xkO_03LE" title="pichy boys" />
-        <LiteYouTube id="cO6NgOGcSD0" title="pichy boys" />
-        <LiteYouTube id="3gQAmehyj6w" title="pichy boys" />
-        <LiteYouTube id="zxtwlk0el7A" title="pichy boys" />
-      </Slider>
-    </div>
+    <Slider {...settings}>
+      <LiteYouTube id="Iw-xkO_03LE" title="pichy boys" />
+      <LiteYouTube id="cO6NgOGcSD0" title="pichy boys" />
+      <LiteYouTube id="3gQAmehyj6w" title="pichy boys" />
+      <LiteYouTube id="zxtwlk0el7A" title="pichy boys" />
+    </Slider>
   );
 }
 

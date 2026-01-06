5c93c62882bc969656823c95ffdffb23418b6acc
fjmg1984 <fjmiranda@tetraimpacts.com>
2026-01-06 18:26:52 -0500

    add path aliases for components & layouts directories

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@layouts": path.resolve(__dirname, "./src/layouts"),
+      },
+    },
+  },
 });
